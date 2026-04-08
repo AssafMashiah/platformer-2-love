c88e34a4fa980955c0f97ca018e481d00fc300df
Gas Assaf <assat@thriv.es>
2026-04-08 15:54:39 +0000

    fix: correct FPS limit in conf.lua

--- conf.lua
+++ conf.lua
@@ -2,7 +2,6 @@ function love.conf(t)
     t.window.title = "Platformer - Survive the Onslaught"
     t.window.width = 800
     t.window.height = 600
+    t.window.fpslimit = 60
     t.console = true
 end
-
-love.setFPSLimit(60)
