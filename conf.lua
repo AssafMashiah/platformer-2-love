31394145231a381dc96206cd5d70fa8f72b77479
Gas Assaf <assat@thriv.es>
2026-04-10 14:53:08 +0000

    Add: conf.lua with vsync (60 FPS limit) and console enabled for debugging

--- conf.lua
+++ conf.lua
@@ -2,4 +2,6 @@ function love.conf(t)
     t.window.title = "Platformer - Survive the Onslaught"
     t.window.width = 800
     t.window.height = 600
+    t.window.vsync = 1
+    t.console = true
 end
